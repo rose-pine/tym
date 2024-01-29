af58f0b37c41cb97db95b6d8eef4f462a9cfbb5c
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2024-01-29 15:58:26 -0800

    Create theme.lua

---
+++ theme.lua
@@ -0,0 +1,27 @@
+local bg = '#1f1d2e'
+local fg = '#e0def4'
+return {
+  color_background = bg,
+  color_foreground = fg,
+  color_bold = fg,
+  color_cursor = fg,
+  color_cursor_foreground = bg,
+  color_highlight = fg,
+  color_highlight_foreground = bg,
+  color_0  = bg,
+  color_1  = '#eb6f92',
+  color_2  = '#31748f',
+  color_3  = '#f6c177',
+  color_4  = '#9ccfd8',
+  color_5  = '#c4a7e7',
+  color_6  = '#ebbcba',
+  color_7  = fg,
+  color_8  = color_1,
+  color_9  = color_2,
+  color_10  = color_3,
+  color_11  = color_4,
+  color_12  = color_5,
+  color_13  = color_6,
+  color_14  = color_7,
+  color_15  = color_8
+}
